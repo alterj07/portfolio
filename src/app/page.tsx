232e971f6d488656a522dda88caea388db526f45
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-24 12:50:10 -0500

    adding navbar to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
-    <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
+    <div className="min-h-screen grid grid-rows-[auto_1fr_auto] bg-black text-white">
       <header className="bg-gray-800 text-white p-4">
         <h1 className="text-2xl font-bold">Welcome to My Website</h1>
       </header>
+      <NavBar />
     </div>
   );
 }
