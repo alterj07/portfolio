8facbc7290fc46c643f77c06cf905ac63e831cfc
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-24 22:22:12 -0500

    adapted to tailwind css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,5 @@
-// import ReactDOM from 'react-dom/client';
-// import React from 'react';
-// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-// import {Link} from 'react-router-dom';
 import Link from 'next/link';
-// import {useRouter} from 'next/router';
 import './NavBar.css';
-// import {AboutMe} from '../Pages/AboutMe.js';
-// import {Projects} from '../Pages/Projects.js';
-// import {MainPage} from '../Pages/MainPage.js';
-// import { fromJSON } from 'postcss';
 export default function NavBar() {
     return (
       <ul class = "display-flex align-items-center flex-direction-column color-burlywood padding-1rem position-absolute left-0 width-14rem height-full list-style-none">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import NavBar from "../Components/NavBar";
+import NavigationBar from '@/Components/NavBar.js';
+import './MainPage.css';
+
 export default function Home() {
   return (
     <div className="min-h-screen grid grid-rows-[auto_1fr_auto] bg-black text-white">
-      <header className="bg-gray-800 text-white p-4">
-        <h1 className="text-2xl font-bold">Welcome to My Website</h1>
-      </header>
-      <NavBar />
+      <div>
+        <NavigationBar />
+      </div>
+      <div className="bg-gray-800 text-white p-4">
+        <h1 className="text-2xl font-bold">Jayden Portfolio</h1>
+      </div>
     </div>
-  );
+  )
 }
+
+
+
