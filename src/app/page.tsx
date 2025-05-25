e1e0ab5f5c7ea60ef85a5c61eb25150c041ed2d9
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-24 22:40:48 -0500

    added global.css import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import NavigationBar from '../Components/NavBar.js';
+import '../globals.css';
 export default function Home() {
   return (
-    <div className="bg-black-900 text-white">
+    <div className="bg-black text-white">
       <div className="bg-gray-800 text-white p-4">
         <h1 className="text-2xl font-bold">Jayden Portfolio</h1>
       </div>
