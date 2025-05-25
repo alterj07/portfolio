8598844ee9552fc2c42403e8228331ebe373f246
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-24 22:11:06 -0500

    fixed class list of navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -12,9 +12,9 @@ import './NavBar.css';
 export default function NavBar() {
     return (
       <ul class = "display-flex align-items-center flex-direction-column color-burlywood padding-1rem position-absolute left-0 width-14rem height-full list-style-none">
-        <li class = "navItems"><Link href ="../Pages/MainPage">Home</Link></li>
-        <li class = "navItems"><Link href ="../Pages/AboutMe">About Me</Link></li>
-        <li class = "navItems"><Link href ="../Pages/Projects">Projects</Link></li>
+        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="../Pages/MainPage">Home</Link></li>
+        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="../Pages/AboutMe">About Me</Link></li>
+        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="../Pages/Projects">Projects</Link></li>
       </ul>
     )
   }
