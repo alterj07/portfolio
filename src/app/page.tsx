9e40aadf4003da2fee1e5e6231eef81330bc380b
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-24 22:36:33 -0500

    fixing tailwind mistakes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import NavigationBar from '../Components/NavBar.js';
-
 export default function Home() {
   return (
-    <div className="bg-black text-white">
+    <div className="bg-black-900 text-white">
       <div className="bg-gray-800 text-white p-4">
         <h1 className="text-2xl font-bold">Jayden Portfolio</h1>
       </div>
