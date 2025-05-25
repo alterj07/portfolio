81bf75e3c7a884a55d30725aa67c9bb092fc4ef9
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-24 22:28:37 -0500

    figuring out tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import NavigationBar from '../Components/NavBar.js';
 
 export default function Home() {
   return (
-    <div className="min-h-screen grid grid-rows-[auto_1fr_auto] bg-black text-white">
-      <div>
-        <NavigationBar />
-      </div>
+    <div className="bg-black text-white">
       <div className="bg-gray-800 text-white p-4">
         <h1 className="text-2xl font-bold">Jayden Portfolio</h1>
       </div>
+        <NavigationBar />
     </div>
   )
 }
