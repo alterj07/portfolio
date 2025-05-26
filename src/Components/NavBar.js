0db809d4ce526c1a9af01b33c4369fa5ea49a8a8
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-26 15:43:04 -0500

    fixing page links

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 export default function NavBar() {
     return (
       <ul class = "display-flex align-items-center flex-direction-column color-burlywood padding-1rem position-absolute left-0 width-14rem height-full list-style-none">
-        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="../Pages/MainPage">Home</Link></li>
-        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="../Pages/AboutMe">About Me</Link></li>
-        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="../Pages/Projects">Projects</Link></li>
+        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="/">Home</Link></li>
+        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="../Pages/AboutMe.js">About Me</Link></li>
+        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="../Pages/Projects.js">Projects</Link></li>
       </ul>
     )
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 export default function Home() {
   return (
     <div className="bg-black text-white">
-      <div className="bg-gray-800 text-white p-4">
-        <h1 className="text-2xl font-bold">Jayden Portfolio</h1>
+      <div className="bg-black text-white p-4">
+        <h1 className="bg-black text-2xl font-bold">Jayden Portfolio</h1>
       </div>
         <NavigationBar />
     </div>
