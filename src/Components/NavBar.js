f17e35d20d68514d8c95905f5c6c83ca02bd0a70
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-26 15:48:40 -0500

    testing next.js linking

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,8 +3,8 @@ export default function NavBar() {
     return (
       <ul class = "display-flex align-items-center flex-direction-column color-burlywood padding-1rem position-absolute left-0 width-14rem height-full list-style-none">
         <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="/">Home</Link></li>
-        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="../Pages/AboutMe.js">About Me</Link></li>
-        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="../Pages/Projects.js">Projects</Link></li>
+        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="@/src/app/Pages/AboutMe">About Me</Link></li>
+        <li class = "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem"><Link href ="@/Pages/Projects">Projects</Link></li>
       </ul>
     )
   }
