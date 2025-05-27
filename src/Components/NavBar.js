1e32468a292863401a9e16fad52710c8a7e5792e
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-27 17:22:17 -0500

    fixing stuff

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,8 +3,8 @@ export default function NavBar() {
     return (
       <ul class = "display-flex align-items-center flex-direction-column color-burlywood padding-1rem position-absolute left-0 width-14rem height-full list-style-none">
         <li><Link className= "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem" href ="/">Home</Link></li>
-        <li><Link className= "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem" href ="@/src/app/Pages/AboutMe">About Me</Link></li>
-        <li><Link className= "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem" href ="@/Pages/Projects">Projects</Link></li>
+        <li><Link className= "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem" href ="../Pages/AboutMe">About Me</Link></li>
+        <li><Link className= "display-flex color-yellowgreen text-decoration-none font-weight-bolder font-size-2-5rem margin-top-2rem" href ="../Pages/Projects">Projects</Link></li>
       </ul>
     )
   }
