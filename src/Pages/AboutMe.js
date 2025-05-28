c76578357eaa0493c0a7ca13e2d8ac939d40e43c
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-27 19:03:53 -0500

    added basic linking functionality in all pages

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,5 +1,8 @@
 export default function AboutMe() {
     return (
-        <h1>About Me!</h1>
+        <div>
+            <h1>About Me!</h1>
+            <Link href = "/">Go back</Link>
+        </div>
     )
 }
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,5 +1,19 @@
 export default function Projects() {
     return (
-        <h1>Projects!</h1>
+        <div>
+            <h1>Projects!</h1>
+            <Link href = "/">Go back</Link>
+
+            <h1>Finished!</h1>
+            <h2><a href = "https://github.com/alterj07/orchestral-pieces">orchestral-pieces</a></h2>
+            <h2><a href = "https://github.com/alterj07/Sprig">Sprig Project</a></h2>
+            <h2><a href = "https://github.com/alterj07/words-of-wisdom">words-of-wisdom</a></h2>
+            <h2><a href = "https://github.com/alterj07/HackPad">HackPad</a></h2>
+            <h2><a href = "https://github.com/alterj07/CookieCrumbl">CookieCrumbl</a></h2>
+            <h1>In-Progress</h1>
+            <h2><a href = "https://github.com/alterj07/PurePrep">PurePrep</a></h2>
+            <h2><a href = "https://github.com/alterj07/portfolio">portfolio</a></h2>
+            <h2><a href = "https://github.com/alterj07/vibe">Vibe</a></h2>
+        </div>
     )
 }
