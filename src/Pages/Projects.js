5e007ba71eee476899764193b4713fe8528d1826
Jayden Chun <jayden.h.chun@gmail.com>
2025-05-27 19:07:14 -0500

    changed project links to open in new tabs

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -6,15 +6,15 @@ export default function Projects() {
             <Link href = "/">Go back</Link>
 
             <h1>Finished!</h1>
-            <h2><a href = "https://github.com/alterj07/orchestral-pieces">orchestral-pieces</a></h2>
-            <h2><a href = "https://github.com/alterj07/Sprig">Sprig Project</a></h2>
-            <h2><a href = "https://github.com/alterj07/words-of-wisdom">words-of-wisdom</a></h2>
-            <h2><a href = "https://github.com/alterj07/HackPad">HackPad</a></h2>
-            <h2><a href = "https://github.com/alterj07/CookieCrumbl">CookieCrumbl</a></h2>
+            <h2><a target="_blank" rel="noopener noreferrer" href = "https://github.com/alterj07/orchestral-pieces">orchestral-pieces</a></h2>
+            <h2><a target="_blank" rel="noopener noreferrer" href = "https://github.com/alterj07/Sprig">Sprig Project</a></h2>
+            <h2><a target="_blank" rel="noopener noreferrer" href = "https://github.com/alterj07/words-of-wisdom">words-of-wisdom</a></h2>
+            <h2><a target="_blank" rel="noopener noreferrer" href = "https://github.com/alterj07/HackPad">HackPad</a></h2>
+            <h2><a target="_blank" rel="noopener noreferrer" href = "https://github.com/alterj07/CookieCrumbl">CookieCrumbl</a></h2>
             <h1>In-Progress</h1>
-            <h2><a href = "https://github.com/alterj07/PurePrep">PurePrep</a></h2>
-            <h2><a href = "https://github.com/alterj07/portfolio">portfolio</a></h2>
-            <h2><a href = "https://github.com/alterj07/vibe">Vibe</a></h2>
+            <h2><a target="_blank" rel="noopener noreferrer" href = "https://github.com/alterj07/PurePrep">PurePrep</a></h2>
+            <h2><a target="_blank" rel="noopener noreferrer" href = "https://github.com/alterj07/portfolio">portfolio</a></h2>
+            <h2><a target="_blank" rel="noopener noreferrer" href = "https://github.com/alterj07/vibe">Vibe</a></h2>
         </div>
     )
 }
