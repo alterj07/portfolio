c5e91c1be3c302b321ee3f2f173b97d8ff6698ef
Jayden Chun <jayden.h.chun@gmail.com>
2025-06-14 23:00:13 -0500

    testing linear gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import "../css/classes.css"
 export default function Home() {
   return (
-    <div className="bg-linear-to-r/srgb from-emerald-950 via-cyan-700 to-black-1000">
+    <div className="bg-linear-to-r/srgb from-indigo-500 to-teal-400">
       <div className="bg-linear-to-r/srgb from-emerald-950 via-cyan-700 to-black-1000 p-4">
         <h1 className="topTitle">Jayden Portfolio</h1>
       </div>
