32a400f4b7bfc7b151239cee25acabe3209015c4
Jayden Chun <jayden.h.chun@gmail.com>
2025-06-14 23:10:20 -0500

    fixed tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import "../css/classes.css"
 export default function Home() {
   return (
     <div className="bg-gradient-to-r from-indigo-500 to-teal-400">
-      <div className="bg-gradient-to-r from-emerald-950 via-cyan-700 to-black p-4">
+      <div className="bg-gradient-to-r from-indigo-500 to-teal-400">
         <h1 className="topTitle">Jayden Portfolio</h1>
       </div>
         <NavigationBar />
