207bfda87dd7382e1ad3ce2fd82c53283d5c2158
Jayden Chun <jayden.h.chun@gmail.com>
2025-06-14 22:54:11 -0500

    changing some bg bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 import "../css/classes.css"
 export default function Home() {
   return (
-    <div className="bg-black">
-      <div className="bg-black p-4">
+    <div className="bg-[var(--color-darkMode-background)]">
+      <div className="bg-[var(--color-darkMode-background)] p-4">
         <h1 className="topTitle">Jayden Portfolio</h1>
       </div>
         <NavigationBar />
