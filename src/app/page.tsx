2bca81cab73ef7a1e8b06c01b74ed5e59cd05c1f
Jayden Chun <jayden.h.chun@gmail.com>
2025-06-14 23:14:05 -0500

    fixed typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import "../css/classes.css"
 export default function Home() {
   return (
-    <div className="h-sreen bg-gradient-to-r from-emerald-950 via-cyan-700 to-black">
+    <div className="h-screen bg-gradient-to-r from-emerald-950 via-cyan-700 to-black">
       <div className="bg-gradient-to-r from-emerald-950 via-cyan-700 to-black">
         <h1 className="topTitle">Jayden Portfolio</h1>
       </div>
