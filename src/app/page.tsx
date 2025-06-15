1f6d0e959cb800741be9ed122a45cd8163ebe5e3
Jayden Chun <jayden.h.chun@gmail.com>
2025-06-14 23:06:29 -0500

    changing linear to gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 import "../css/classes.css"
 export default function Home() {
   return (
-    <div className="bg-linear-to-r from-indigo-500 to-teal-400">
-      <div className="bg-linear-to-r from-emerald-950 via-cyan-700 to-black p-4">
+    <div className="bg-gradient-to-r from-indigo-500 to-teal-400">
+      <div className="bg-gradient-to-r from-emerald-950 via-cyan-700 to-black p-4">
         <h1 className="topTitle">Jayden Portfolio</h1>
       </div>
         <NavigationBar />
