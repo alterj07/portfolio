c5e3de29a04cc36f65f4ae85a7ef141327508788
Jayden Chun <jayden.h.chun@gmail.com>
2025-06-14 23:16:12 -0500

    testing gradients

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import './globals.css';
 import "../css/classes.css"
 export default function Home() {
   return (
-    <div className="h-screen bg-gradient-to-r from-emerald-950 via-cyan-700 to-black">
-      <div className="bg-gradient-to-r from-emerald-950 via-cyan-700 to-black">
+    <div className="h-screen bg-gradient-to-br from-[#0f0c29] via-[#302b63] to-[#24243e]
+">
+      {/* <div className="bg-gradient-to-r from-emerald-950 via-cyan-700 to-black"> */}
+      <div>
         <h1 className="topTitle">Jayden Portfolio</h1>
       </div>
         <NavigationBar />
