165dbe6076401497e8ef0b3c3e71113b1a8b679d
Jayden Chun <jayden.h.chun@gmail.com>
2025-06-18 06:47:52 -0500

    redesigned the website + attempted to use scroll-timeline and scroll-drive-animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,21 @@ import "../css/classes.css"
 export default function Home() {
   return (
     // <div className="h-screen bg-gradient-to-br from-[#4ecdc4] to-[#556270] scroll-smooth">
-    <div className = "h-screen bg-black scroll-smooth">
+    <div className = "bg-black scroll-smooth">
+      <div className = "mainTitleSpacerDiv"></div>
       <div>
-        <h1 className="mainPageTitle">Jayden&apos;s Portfolio</h1>
+        <h1 className="mainPageTitle">Jayden Chun</h1>
       </div>
         <NavigationBar />
-      
+      <div className = "sectionSpacerDiv"></div>
+      <div>
+        <h1 id = "featuredProjectsTitle" className = "test-scroll-animation">
+          Featured Projects
+        </h1>
+      </div>
+      <div className = "h-screen">
+
+      </div>
     </div>
   )
 }
