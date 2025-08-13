e6341f107bfa3fa262efed0c7fbb99c0273db36d
Jayden Chun <jayden.h.chun@gmail.com>
2025-08-12 22:34:02 -0500

    created more stars + redesigned the content of the stars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,29 @@ import './globals.css';
 import "../css/classes.css"
 export default function Home() {
   return (
-    <div className = "bg-black scroll-smooth">
-      <div className = "mainTitleSpacerDiv" />
-      <div>
-        <h1 className="mainPageTitle">Jayden Chun</h1>
-        <p id = "mainPageDescription">Student by Day, Coder by Night</p>
-      </div>
+    <div className="bg-black scroll-smooth">
+      <div className="mainTitleSpacerDiv" />
+      <div style={{ position: 'relative', width: '100vw', height: '60vh', minHeight: 400, marginBottom: 32 }}>
+        {/* Centered main text */}
+        <div style={{
+          position: 'absolute',
+          top: '20%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 10,
+          textAlign: 'center',
+        }}>
+          <h1 className="mainPageTitle">Jayden Chun</h1>
+          <p id="mainPageDescription">Student by Day, Coder by Night</p>
+        </div>
+        {/* Scattered stars around the center text */}
         <Stars />
-        <NavigationBar />
-      <div className = "sectionSpacerDiv" />
-        <FeatureProjects />
-        <Footer />
+      </div>
+      <NavigationBar />
+      <div className="sectionSpacerDiv" />
+      <FeatureProjects />
+      <Footer />
     </div>
-  )
+  );
 }
 
--- src/components/stars/page.tsx
+++ src/components/stars/page.tsx
@@ -1,49 +1,43 @@
-import React from 'react'
-import "../../css/stars.css"
+import React from 'react';
+import "../../css/stars.css";
+
 export default function Stars() {
     return (
-        <div>
-            <WhoIAmStar/>
-            <LanguagesStar/>
+        <div style={{ position: 'relative', width: '100vw', height: '100vh', minHeight: 600 }}>
+            {/* Scattered stars around the center text */}
+            <StarWithPrompt
+                style={{ top: '30%', left: '25%' }}
+                prompt="I am a 17 year-old enthusiastic developer who likes to develop apps, websites, and create any projects my heart desires. I have participated in events like a 28-long hackathon, regional CS UIL Competitions, ... My passion is spread out between playing my cello, competing in tennis, and being a developer/engineer."
+            />
+            <StarWithPrompt
+                style={{ top: '25%', left: '65%' }}
+                prompt={<span>My repetoire of languages:<ul><li>Java</li><li>Python</li><li>JavaScript</li><li>Swift</li><li>English</li><li>Korean</li><li>Spanish</li></ul></span>}
+            />
+            <StarWithPrompt
+                style={{ top: '60%', left: '30%' }}
+                prompt={<span>Data Structures & Algorithms:<ul><li>Binary Trees</li><li>Graphs</li><li>Dynamic Programming</li><li>Sorting & Searching</li><li>Big O Analysis</li></ul></span>}
+            />
+            <StarWithPrompt
+                style={{ top: '65%', left: '60%' }}
+                prompt={<span>System Design:<ul><li>REST APIs</li><li>Microservices</li><li>Database Design</li><li>Scalability</li></ul></span>}
+            />
+            <StarWithPrompt
+                style={{ top: '45%', left: '80%' }}
+                prompt={<span>Soft Skills:<ul><li>Communication</li><li>Teamwork</li><li>Problem Solving</li><li>Leadership</li></ul></span>}
+            />
+            <StarWithPrompt
+                style={{ top: '70%', left: '50%' }}
+                prompt={<span>Projects:<ul><li>Hackathons</li><li>Open Source</li><li>Personal Portfolio</li></ul></span>}
+            />
         </div>
-    )
+    );
 }
 
-function WhoIAmStar() {
+function StarWithPrompt({ style, prompt }: { style: React.CSSProperties, prompt: React.ReactNode }) {
     return (
-        <div>
-            <div id = "WhoIAmStar" className="starShape"/>
-            <div id = "WhoIAm" className = "starContent">
-                <span>
-                I am a 17 year-old enthusiastic developer who
-                    likes to develop apps, websites, and create any projects my heart desires.
-                    I have participated in events like a 28-long hackathon, regional CS UIL Competitions, ...
-                    My passion is spread out between playing my cello, competing in tennis, and being a developer/engineer.
-                </span>
-            </div>
+        <div className="starPromptRow" style={{ position: 'absolute', ...style, pointerEvents: 'none' }}>
+            <div className="starShape" tabIndex={0} style={{ pointerEvents: 'auto' }} />
+            <div className="starContent">{prompt}</div>
         </div>
-    )
-}
-
-function LanguagesStar() {
-    return (
-        <div>
-            <div id = "LanguagesStar" className = "starShape"/>
-            <div id = "Languages" className = "starContent">
-                <span>
-                    My repetoire of - fluent or kind of fluent - languages so far:
-                    <br/>
-                    <ul>
-                        <li>Java</li>
-                        <li>Python</li>
-                        <li>Some Javascript....</li>
-                        <li>Some Swift...</li>
-                        <li>English</li>
-                        <li>Korean</li>
-                        <li>Un poco Espanol</li>
-                    </ul>
-                </span>
-            </div>
-        </div>
-    )
+    );
 }
