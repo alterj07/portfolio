ed22940d9099d927560d25559315ad3f05de98a3
Jayden Chun <jayden.h.chun@gmail.com>
2025-08-12 22:40:26 -0500

    moved up the stars around the center text thing

--- src/components/stars/page.tsx
+++ src/components/stars/page.tsx
@@ -6,27 +6,27 @@ export default function Stars() {
         <div style={{ position: 'relative', width: '100vw', height: '100vh', minHeight: 600 }}>
             {/* Scattered stars around the center text */}
             <StarWithPrompt
-                style={{ top: '30%', left: '25%' }}
+                style={{ top: '-20%', left: '25%' }}
                 prompt="I am a 17 year-old enthusiastic developer who likes to develop apps, websites, and create any projects my heart desires. I have participated in events like a 28-long hackathon, regional CS UIL Competitions, ... My passion is spread out between playing my cello, competing in tennis, and being a developer/engineer."
             />
             <StarWithPrompt
-                style={{ top: '25%', left: '65%' }}
+                style={{ top: '-15%', left: '65%' }}
                 prompt={<span>My repetoire of languages:<ul><li>Java</li><li>Python</li><li>JavaScript</li><li>Swift</li><li>English</li><li>Korean</li><li>Spanish</li></ul></span>}
             />
             <StarWithPrompt
-                style={{ top: '60%', left: '30%' }}
+                style={{ top: '10%', left: '30%' }}
                 prompt={<span>Data Structures & Algorithms:<ul><li>Binary Trees</li><li>Graphs</li><li>Dynamic Programming</li><li>Sorting & Searching</li><li>Big O Analysis</li></ul></span>}
             />
             <StarWithPrompt
-                style={{ top: '65%', left: '60%' }}
+                style={{ top: '15%', left: '60%' }}
                 prompt={<span>System Design:<ul><li>REST APIs</li><li>Microservices</li><li>Database Design</li><li>Scalability</li></ul></span>}
             />
             <StarWithPrompt
-                style={{ top: '45%', left: '80%' }}
+                style={{ top: '5%', left: '80%' }}
                 prompt={<span>Soft Skills:<ul><li>Communication</li><li>Teamwork</li><li>Problem Solving</li><li>Leadership</li></ul></span>}
             />
             <StarWithPrompt
-                style={{ top: '70%', left: '50%' }}
+                style={{ top: '30%', left: '50%' }}
                 prompt={<span>Projects:<ul><li>Hackathons</li><li>Open Source</li><li>Personal Portfolio</li></ul></span>}
             />
         </div>
