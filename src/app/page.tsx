2adc2545a237e9b15b7d69872ac2f181334249ec
Jayden Chun <jayden.h.chun@gmail.com>
2025-08-22 00:01:50 -0500

    changed format of the portfolio + added a few text components to the beginning of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
 import NavigationBar from '../components/navBar/page';
 import FeatureProjects from '../components/projectsFeature/page';
 import Footer from '../components/footer/page';
+import Introduction from '../components/introduction/page';
 import './globals.css';
-import "../css/classes.css"
+import "../css/classes.css" 
 export default function Home() {
   return (
     <div className="bg-black scroll-smooth">
-      <div className="mainTitleSpacerDiv" />
-      <div style={{ position: 'relative', width: '100vw', height: '60vh', minHeight: 400, marginBottom: 32 }}>
-        {/* Centered main text */}
-        <div style={{
-          position: 'absolute',
-          top: '20%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          zIndex: 10,
-          textAlign: 'center',
-        }}>
-          <h1 className="mainPageTitle">Jayden Chun</h1>
-          <p id="mainPageDescription">Student by Day, Coder by Night</p>
-        </div>
+      <div>
+        <Introduction />
       </div>
       <NavigationBar />
       <div className="sectionSpacerDiv" />
---
+++ src/components/introduction/page.tsx
@@ -0,0 +1,12 @@
+import "../../css/classes.css" 
+import '../../css/introduction.css'
+export default function Introduction() {
+    return (
+        <div id = "introductionContainer">
+            <div id = "nameContainer">
+                <h1 id="name">Jayden Chun</h1>
+                <p id="description">Class of 2026<br/>Aspiring Engineer<br/>Passionate Cellist</p>
+            </div>
+        </div>
+    )
+}
