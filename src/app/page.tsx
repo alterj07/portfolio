b33010c14541426541a6c5f03e674562004a8806
Jayden Chun <jayden.h.chun@gmail.com>
2025-08-21 23:36:37 -0500

    deleted all star components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import NavigationBar from '../components/navBar/page';
 import FeatureProjects from '../components/projectsFeature/page';
 import Footer from '../components/footer/page';
-import Stars from '../components/stars/page';
 import './globals.css';
 import "../css/classes.css"
 export default function Home() {
@@ -21,8 +20,6 @@ export default function Home() {
           <h1 className="mainPageTitle">Jayden Chun</h1>
           <p id="mainPageDescription">Student by Day, Coder by Night</p>
         </div>
-        {/* Scattered stars around the center text */}
-        <Stars />
       </div>
       <NavigationBar />
       <div className="sectionSpacerDiv" />
