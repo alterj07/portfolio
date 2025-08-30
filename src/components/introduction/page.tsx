faa89fc9eac7aa409a9cfa8bfb44ef2df67ec886
Jayden Chun <jayden.h.chun@gmail.com>
2025-08-29 23:24:41 -0500

    added a hovering pfp animation

--- src/components/introduction/page.tsx
+++ src/components/introduction/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "../../css/classes.css" 
 import '../../css/introduction.css'
 import ProfilePic from '../../assets/pfp.jpg'
+import HiddenPic from '../../assets/pfp-hover.jpg'
 export default function Introduction() {
     return (
         <div id = "introductionContainer">
@@ -11,6 +12,7 @@ export default function Introduction() {
             </div>
             <div id = "pictureContainer">
                 <img src = {ProfilePic.src} alt="Jayden Chun" id = "profilePicture"/>
+                <img src = {HiddenPic.src} alt="Jayden Chun" id = "hiddenProfilePicture"/>
             </div>
         </div>
     )
