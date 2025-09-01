35c69dc41b772bcea108d962b84a64df48ab10d9
Jayden Chun <jayden.h.chun@gmail.com>
2025-09-01 17:59:20 -0500

    added more timeline components

--- src/components/timeline/page.tsx
+++ src/components/timeline/page.tsx
@@ -12,7 +12,15 @@ export default function TimelineComponent(){
     return (
         <div id = "timelineContainer">
             <Timeline position="alternate" id = "timeline">
-                <TimelineItemContent date = "June 2025 - Current" name = "Intern - Silvia" description = {["Description", "Description 2"]}/>
+                <TimelineItemContentRight date = "June 2025 - Current" name = "Intern - Silvia" 
+                description = {["Contributed to the design and testing of a multi-domain mobile technology program for dementia prevention.",
+                "Participated in multilingual translations, community outreach, and recruitment of study participants.",
+                "Aided in data collection to examine the effectiveness of digital tools in reducing dementia risk."]}/>
+
+                <TimelineItemContentLeft date = "August 2024 - June 2025" name = "Intern - NASA HAS Aerospace Scholars" 
+                description = {["Completed NASA Aerospace Scholars program modules with emphasis on engineering, CAD modeling, and systems design.",
+                "Applied CAD, coding, and systems engineering skills in a rigorous Moonshot mission simulation with NASA engineers.",
+                "Participated in a year-long program, researching interstellar bodies, designing aerospace tools and habitats, creating Scratch programs, and presenting findings to professors and peers."]}/>
             </Timeline>
         </div>
     )
@@ -26,7 +34,7 @@ type TimelineItemContentProps = {
     description: string[];
 };
 
-function TimelineItemContent({ date, name, description }: TimelineItemContentProps) {
+function TimelineItemContentRight({ date, name, description }: TimelineItemContentProps) {
     return (
         <TimelineItem className = "timeline-item">
             <TimelineOppositeContent>
@@ -38,7 +46,29 @@ function TimelineItemContent({ date, name, description }: TimelineItemContentPro
             </TimelineSeparator>
             <TimelineContent>
                 <h3>{name}</h3>
-                <ul>
+                <ul className = "timeline-description">
+                    {description.map((desc, index) => (
+                        <li key={index}>{desc}</li>
+                    ))}
+                </ul>
+            </TimelineContent>
+        </TimelineItem>
+    )
+}
+
+function TimelineItemContentLeft({ date, name, description }: TimelineItemContentProps) {
+    return (
+        <TimelineItem className = "timeline-item">
+            <TimelineOppositeContent>
+                {date}
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+                <h3>{name}</h3>
+                <ul className = "timeline-description">
                     {description.map((desc, index) => (
                         <li key={index}>{desc}</li>
                     ))}
