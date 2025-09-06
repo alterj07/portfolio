bc711b3f3dd62397cf89a961d08e080cb55fc563
Jayden Chun <jayden.h.chun@gmail.com>
2025-09-06 12:05:37 -0500

    fixed the intro page on both mobile + computer

--- src/components/introduction/page.tsx
+++ src/components/introduction/page.tsx
@@ -12,7 +12,6 @@ export default function Introduction() {
             </div>
             <div id = "nameContainer">
                 <h1 id="name">Jayden Chun</h1>
-                <p id = "class">Class of &apos;26</p>
                 <p id="description">Aspiring Engineer<br/>Passionate Cellist</p>
             </div>
         </div>
