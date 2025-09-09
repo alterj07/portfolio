2d96936225874a974e1edbb2fc1b53b334106108
Jayden Chun <jayden.h.chun@gmail.com>
2025-09-08 19:04:27 -0500

    fixed eslint no use error

--- src/components/scrollDownButton/page.tsx
+++ src/components/scrollDownButton/page.tsx
@@ -3,8 +3,19 @@
 import React from 'react'
 import '../../css/introduction.css'
 import { FaAngleDown } from "react-icons/fa6";
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * A component that creates a scroll-down button.
+ *
+ * The component will smoothly scroll down by 90% of the window's height when clicked.
+ *
+ * Props:
+ *   - None
+ *
+ * Example:
+ *   
+/*******  fcdc7068-122f-44a9-b4fc-366e7a26b206  *******/
 export default function ScrollDownButton() {
-    interface ScrollDownButtonProps {}
 
     interface ScrollEvent extends React.MouseEvent<HTMLDivElement, MouseEvent> {}
 
