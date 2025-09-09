08518a37d5eb4a9674833a37ac05f65755c984cb
Jayden Chun <jayden.h.chun@gmail.com>
2025-09-08 19:09:42 -0500

    tried to fix remaining eslint error

--- src/components/introduction/page.tsx
+++ src/components/introduction/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import "../../css/classes.css" 
 import '../../css/introduction.css'
 import ProfilePic from '../../assets/2.png'
@@ -10,8 +11,8 @@ export default function Introduction() {
     return (
         <div id = "introductionContainer">
             <div id = "pictureContainer">
-                <img src = {ProfilePic.src} alt="Jayden Chun" id = "profilePicture"/>
-                <img src = {HiddenPic.src} alt="Jayden Chun" id = "hiddenProfilePicture"/>
+                <Image src = {ProfilePic.src} alt="Jayden Chun" id = "profilePicture"/>
+                <Image src = {HiddenPic.src} alt="Jayden Chun" id = "hiddenProfilePicture"/>
             </div>
             <div id = "verticalContainer">
                 <div id = "nameContainer">
--- src/components/scrollDownButton/page.tsx
+++ src/components/scrollDownButton/page.tsx
@@ -3,27 +3,14 @@
 import React from 'react'
 import '../../css/introduction.css'
 import { FaAngleDown } from "react-icons/fa6";
-/*************  ✨ Windsurf Command ⭐  *************/
-/**
- * A component that creates a scroll-down button.
- *
- * The component will smoothly scroll down by 90% of the window's height when clicked.
- *
- * Props:
- *   - None
- *
- * Example:
- *   
-/*******  fcdc7068-122f-44a9-b4fc-366e7a26b206  *******/
 export default function ScrollDownButton() {
 
-    interface ScrollEvent extends React.MouseEvent<HTMLDivElement, MouseEvent> {}
+    // interface ScrollEvent extends React.MouseEvent<HTMLDivElement, MouseEvent> {}
 
-    const handleScrollDown = (e: ScrollEvent): void => {
-        e.preventDefault();
+    const handleScrollDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+        // e.preventDefault();
 
         window.scrollBy({
-            // top: window.innerHeight,
             top: window.innerHeight * 0.9,
             behavior: 'smooth'
         });
